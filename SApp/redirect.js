2f78d5c71edbfa182cc8630dbda15fe41a0f9c75
simfzyzzlmao <jesidaelo@gmail.com>
2024-06-07 21:29:11 +0800

    Update redirect.js
    Fix

--- SApp/redirect.js
+++ SApp/redirect.js
@@ -2,13 +2,13 @@ function finish() {
     var r = document.getElementById("Name").value;
     var rr = document.getElementById("Name").value;
     if (r === "Jesimiel") {
-        alert('You like Kimchelle, hehe i know you!');
+        alert('You like Kimchelle, hehe i know you!')
     }
     if (rr === "Jaylo") {
         alert('Musta Adelyza?')
     }
     else
     {
-        alert('Miss mona?')
+        alert('Di kita kilala.')
     }
 }
