4018c7069ea01d1dba75f8065b8d488d229bcdb6
Jesimiel <jesidaelo@gmail.com>
2024-06-07 23:00:21 +0800

    Update redirect.js

--- SApp/redirect.js
+++ SApp/redirect.js
@@ -1,14 +1,8 @@
 function finish() {
     var r = document.getElementById("Name").value;
     var rr = document.getElementById("Name").value;
-    if (r === "Jesimiel") {
-        alert('You like Kimchelle, hehe i know you!')
-    }
-    if (rr === "Jaylo") {
-        alert('Musta Adelyza?')
-    }
     else
     {
-        alert('Di kita kilala.')
+        alert('Not yet made.')
     }
 }
